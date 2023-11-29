be196d0105ba63ab6816f31fafb28b6ed464b082
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2023-11-29 21:05:53 +0300

    Add files via upload

---
+++ ConvertTheTemperature.java
@@ -0,0 +1,9 @@
+public class ConvertTheTemperature {
+    public double[] convertTemperature(double celsius) {
+        double [] temp=new double[2];
+        temp[0]=celsius+273.15;
+        temp[1]=celsius*1.80+32.00;
+        return  temp;
+
+
+    }}
