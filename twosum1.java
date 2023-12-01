2b869ea6ca0c3fbc4330fab042b88f87741e182d
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2023-12-01 12:10:41 +0300

    Add files via upload

---
+++ twosum1.java
@@ -0,0 +1,21 @@
+public class twosum1 {
+    public int[] twoSum(int[] nums, int target) {
+        int sum=0;
+        int[] arr=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    arr[0]=i;
+                    arr[1]=j;
+
+                    break;
+                }
+            }
+        }
+        return arr;
+
+    }
+}
