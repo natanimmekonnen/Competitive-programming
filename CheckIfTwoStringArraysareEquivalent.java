1640bf1015c6496da2cbc9d3f61e93375474bd92
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-02-28 23:53:29 +0300

    Add files via upload

---
+++ CheckIfTwoStringArraysareEquivalent.java
@@ -0,0 +1,12 @@
+public class CheckIfTwoStringArraysareEquivalent {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String w1 = "";
+        String w2 = "";
+        for (String i : word1)
+            w1 += i;
+        for (String i : word2)
+            w2 += i;
+        return w1.equals(w2);
+
+    }
+}
