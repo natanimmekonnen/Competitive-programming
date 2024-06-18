333ff4b0fe29e2f683f14f49de899b5fe51c4f0a
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-06-18 19:51:23 +0300

    Created 'Weird' by A2SV Hub

---
+++ 18-Jun-2024/Weird 169604.py
@@ -0,0 +1,22 @@
+# Problem: Weird - https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 !=0:
+        print('Weird')
+    elif n<=5:
+        print('Not Weird')
+    elif n<=20:
+        print('Weird')
+    elif n>20:
+        print('Not Weird')
