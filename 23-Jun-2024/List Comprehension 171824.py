3e33c68149d5f10859bf28060351ac6b0b928938
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-06-23 22:04:40 +0300

    Created 'List Comprehension' by A2SV Hub

---
+++ 23-Jun-2024/List Comprehension 171824.py
@@ -0,0 +1,9 @@
+# Problem: List Comprehension - https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    coor = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k!=n]
+    print(coor)
