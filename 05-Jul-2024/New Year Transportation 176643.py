c709a088c7f239a30477866ae01540377bcc7a4d
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-05 19:21:50 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 05-Jul-2024/New Year Transportation 176643.py
@@ -0,0 +1,18 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+
+n, t = map(int, input().split())
+portals = list(map(int, input().split()))
+
+current_position = 1
+
+
+while current_position < t:
+    current_position += portals[current_position - 1]
+    
+    if current_position == t:
+        print("YES")
+        break
+else:
+   
+    print("NO")
