b6e8228ebbbab299ed48785b6d66e66ae52fec12
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-09 22:39:32 +0300

    Created 'Heap Sort' by A2SV Hub

---
+++ 09-Jul-2024/Heap Sort 178279.py
@@ -0,0 +1,9 @@
+# Problem: Heap Sort - https://practice.geeksforgeeks.org/problems/heap-sort/1
+
+Runtime Error 
+Hangup (SIGHUP)
+Traceback (most recent call last):
+  File "Solution.py", line 60, in <module>
+    Solution().HeapSort(arr,n)
+  File "Solution.py", line 25, in HeapSort
+    buildHeap(self,arr, n)
