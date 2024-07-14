cd2b9885356137fb28522401d3332760b3a19bd8
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-14 23:21:35 +0300

    Created 'Find K Pairs with Smallest Sums' by A2SV Hub

---
+++ 14-Jul-2024/Find K Pairs with Smallest Sums 180392.py
@@ -0,0 +1,29 @@
+# Problem: Find K Pairs with Smallest Sums - https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/
+
+class Solution:
+    def kSmallestPairs(
+        self, nums1: List[int], nums2: List[int], k: int
+    ) -> List[List[int]]:
+        if not nums1 or not nums2:
+            return []
+        heap = []
+        visited = set()
+
+        heapq.heappush(heap, (nums1[0] + nums2[0], 0, 0))
+        visited.add((0, 0))
+
+        result = []
+
+        while heap and len(result) < k:
+            sum_, i, j = heapq.heappop(heap)
+            result.append([nums1[i], nums2[j]])
+
+            if i + 1 < len(nums1) and (i + 1, j) not in visited:
+                heapq.heappush(heap, (nums1[i + 1] + nums2[j], i + 1, j))
+                visited.add((i + 1, j))
+
+            if j + 1 < len(nums2) and (i, j + 1) not in visited:
+                heapq.heappush(heap, (nums1[i] + nums2[j + 1], i, j + 1))
+                visited.add((i, j + 1))
+
+        return result
