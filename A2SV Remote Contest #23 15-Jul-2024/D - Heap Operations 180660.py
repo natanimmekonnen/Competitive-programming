ecb35aec0bd732657fe802b81b4b5041f8f581c6
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-15 22:36:27 +0300

    Created 'D - Heap Operations' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/D - Heap Operations 180660.py
@@ -0,0 +1,31 @@
+# Problem: D - Heap Operations - https://codeforces.com/gym/535309/problem/D
+
+from heapq import *
+
+n = int(input())
+heap = []
+op = []
+for i in range(n):
+    v = input().rstrip()
+    if v[0] == 'i':
+        X = int(v.split()[1])
+        heappush(heap, X)
+        op.append(v)
+        continue
+    if v[0] == 'g':
+        X = int(v.split()[1])
+        while heap and heap[0] < X:
+            heappop(heap)
+            op.append("removeMin")
+        if not heap or heap[0] > X:
+            op.append("insert " + str(X))
+            heappush(heap, X)
+        op.append(v)
+        continue
+
+    if not heap:
+        op.append("insert 1")
+    else: heappop(heap)
+    op.append(v)
+print(len(op))
+print("\n".join(op))
