6c0d0130c03ecb9cab626ca582921f9e3b16159f
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-29 21:20:08 +0300

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/A - Valid Parentheses 185733.py
@@ -0,0 +1,18 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+n=input()
+stack=[]
+count=0
+for i, char in enumerate(n):
+    # print(i , char)
+    if char == '(':
+        stack.append(i)
+    else:
+        if stack and n[stack[-1]] == '(':
+            stack.pop()
+            count+=2
+        else:
+            stack.append(i)
+
+
+print(count)
