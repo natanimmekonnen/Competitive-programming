c457890c28e8e5cf7f6df4a80d33bd0370d9eb88
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-29 21:53:14 +0300

    Created 'B - Chef Jamie' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/B - Chef Jamie 185765.py
@@ -0,0 +1,22 @@
+# Problem: B - Chef Jamie - https://codeforces.com/gym/537575/problem/B
+
+def min_swaps(arr):
+    n = len(arr)
+    sorted_arr = sorted(arr)
+    index_map = {value: idx for idx, value in enumerate(arr)}
+    swaps = 0
+
+    for i in range(n):
+        while arr[i] != sorted_arr[i]:
+            swap_idx = index_map[sorted_arr[i]]
+            arr[i], arr[swap_idx] = arr[swap_idx], arr[i]
+            index_map[arr[swap_idx]] = swap_idx
+            index_map[arr[i]] = i
+            swaps += 1
+
+    return swaps
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+print(min_swaps(arr))
