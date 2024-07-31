0d2b90a9baa4943978aea679face0e8ce70e98eb
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-07-31 21:42:54 +0300

    Created 'Odd Sum' by A2SV Hub

---
+++ 31-Jul-2024/Odd Sum 186332.py
@@ -0,0 +1,22 @@
+# Problem: Odd Sum - https://codeforces.com/problemset/problem/797/B
+
+def find_max_odd_sum(arr):
+    possum = sum(x for x in arr if x > 0)
+    odd_candidates = [x for x in arr if x % 2 != 0]
+
+    if possum % 2 != 0:
+        return possum
+
+    smallest_odd_adjustment = odd_candidates[0]
+    for num in odd_candidates:
+        if abs(num) < abs(smallest_odd_adjustment):
+            smallest_odd_adjustment = num
+
+    return possum - abs(smallest_odd_adjustment)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = find_max_odd_sum(arr)
+
+print(result)
