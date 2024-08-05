7ecbda9aa9b54a95798831cca3b4488097b54147
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-08-05 20:50:58 +0300

    Created 'C - Cyclic Components' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/C - Cyclic Components 187603.py
@@ -0,0 +1,34 @@
+# Problem: C - Cyclic Components - https://codeforces.com/gym/540348/problem/C
+
+from collections import  deque
+def  bfs(source):
+    queue=deque([source])
+
+    visited[source]=1
+    booll=True
+    while queue:
+        curr=queue.popleft()
+        if len(graph[curr])!=2:
+            booll=False
+        for i in graph[curr]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=1
+    return booll
+
+
+
+x,y=map(int,input().split())
+visited=[0 for i in range(x)]
+val=0
+graph=[[] for i in range(x)]
+for i in range(y):
+    n,m=map(int,input().split())
+    graph[n-1].append(m-1)
+    graph[m-1].append(n-1)
+# print("ho")
+for i in range(x):
+    if not visited[i] and bfs(i):
+        val+=1
+        # print("ho")
+print(val)
