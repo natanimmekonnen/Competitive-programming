1eddc86f374cb0a6124d444f8ad34098ff58e35b
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-08-06 20:44:32 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 06-Aug-2024/Fibonacci Number 187880.py
@@ -0,0 +1,15 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def __init__(self):
+        self.memo = {0: 0, 1: 1}
+    def fib(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+        
+        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        
+        return self.memo[n]
+        
+
+        
