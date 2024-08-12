ab1975a986e3971581b641e93a88f687eb7573f1
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-08-12 21:11:58 +0300

    Created 'C - Collecting Game' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 12-Aug-2024/C - Collecting Game 190939.py
@@ -0,0 +1,25 @@
+# Problem: C - Collecting Game - https://codeforces.com/gym/541399/problem/C
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    arr = []
+    sum = 0
+    for i in range(n):
+        arr.append([a[i], i])
+        sum += a[i]
+    arr.sort()
+    ans = [0] * n
+    ans[arr[-1][1]] = n - 1
+    sum -= arr[-1][0]
+    for i in range(n - 2, -1, -1):
+        if sum >= arr[i + 1][0]:
+            ans[arr[i][1]] = ans[arr[i + 1][1]]
+        else:
+            ans[arr[i][1]] = i
+        sum -= arr[i][0]
+    for i in range(len(ans)):
+        print(ans[i], end=' ')
+    print()
