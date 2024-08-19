7d686924986f1539c835392ac35289a4a7b4978a
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-08-19 20:28:37 +0300

    Created 'C - Hello World' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 19-Aug-2024/C - Hello World 197927.py
@@ -0,0 +1,20 @@
+# Problem: C - Hello World - https://codeforces.com/gym/543262/problem/C
+
+x = int(input())
+for i in range(x):
+    n = int(input())
+    y = list(map(int, input().split()))
+    y.sort()
+    flag = True
+    sum = 1
+    if y[0] != 1:
+        flag = False
+    for i in range(1, len(y)):
+        if y[i] > sum:
+            flag = False
+            break
+        sum += y[i]
+    if flag:
+        print("Yes")
+    else:
+        print("No")
