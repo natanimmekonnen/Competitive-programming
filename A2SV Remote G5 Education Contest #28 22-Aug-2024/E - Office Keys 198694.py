f4700f8bb9c99ead7c05fea71692b846253c842c
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-08-22 21:30:51 +0300

    Created 'E - Office Keys' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 22-Aug-2024/E - Office Keys 198694.py
@@ -0,0 +1,27 @@
+# Problem: E - Office Keys - https://codeforces.com/gym/543262/problem/E
+
+n,k,p=list(map(int, input().split()))
+peop=sorted(list(map(int,input().split())))
+key=sorted(list(map(int,input().split())))
+
+
+def check(x):
+    keys=0
+    for i in range(n):
+        if keys >= k:
+            return False
+        while abs(peop[i]-key[keys])+abs(p-key[keys])>x:
+            keys+=1
+            if keys>=k:
+                return False
+        keys+=1
+    return True
+left=0
+right=int(1e18)
+while left< right:
+    mid=(left+right)//2
+    if check(mid):
+        right=mid
+    else:
+        left=mid+1
+print(left)
