579f64a1029a3509a53cded22835004651248ee7
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-10-07 22:40:08 +0300

    Created 'D -  Longest Good Sequence' by A2SV Hub

---
+++ A2SV Remote Contest #32 07-Oct-2024/D -  Longest Good Sequence 218718.py
@@ -0,0 +1,20 @@
+# Problem: D -  Longest Good Sequence - https://codeforces.com/gym/555625/problem/D
+
+lim = 111111
+p = [0] * lim
+t = {}
+t[1] = [1]
+for i in range(2, lim):
+    if t.get(i,0) == 0:
+        t[i] = [i]
+        for j in range(2 * i, lim, i):
+        	if j not in t:
+        		t[j] = []
+        	t[j].append(i)
+input()
+a = list(map(int, input().split()))
+for i in a:
+    x = max(p[j] for j in t[i]) + 1
+    for j in t[i]:
+    	p[j] = x
+print(max(p))
