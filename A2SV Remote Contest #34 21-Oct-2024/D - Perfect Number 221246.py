994ecd9b4fc0a831a1c911484a08c4483dd1184f
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-10-21 22:48:51 +0300

    Created 'D - Perfect Number' by A2SV Hub

---
+++ A2SV Remote Contest #34 21-Oct-2024/D - Perfect Number 221246.py
@@ -0,0 +1,24 @@
+# Problem: D - Perfect Number - https://codeforces.com/gym/559568/problem/D
+
+k = int(input())
+
+n = 19
+count = 0
+z = 1
+
+while True:
+    y = 0
+    x = n
+
+    while x > 0:
+        y += x % 10
+        x //= 10
+
+    if y == 10:
+        count += 1
+        if count == k:
+            print(n)
+            break
+
+    z += 1
+    n += 1
