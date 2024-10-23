0693cf1e3e21abdb93f8d17e433e11cc9a28c004
natanimmekonnen <115899178+natanimmekonnen@users.noreply.github.com>
2024-10-23 20:17:48 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 23-Oct-2024/Complicated GCD 221818.py
@@ -0,0 +1,12 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+def complicated_gcd(a_str, b_str):
+    if a_str == b_str:
+        print(a_str)
+    else:
+        print(1)
+
+
+a_str, b_str = input().split()
+
+complicated_gcd(a_str, b_str)
